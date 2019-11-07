83816815d6c260c097ac48bcb7e1ef5aef3eafda
nkh1988 <nkh1988@naver.com>
2019-11-07 15:37:12 +0900

    공지사항 및 자료실 마크업과 디자인 및 작업

--- js/webcafe.js
+++ js/webcafe.js
@@ -1,11 +1,16 @@
 var menu_item = $('.menu > li');
 var btn_menu = $('.menu > li > a');
 var sub_link = $('.sub-menu a');
+var tab_list_link = $('.board li a');
+var tab_section = $('.board section');
+var tab =$('.tab a');
 
 menu_item.addClass('menu-item');
 btn_menu.addClass('btn-menu');
 sub_link.addClass('icon-dot-circled');
+tab_list_link.addClass('icon-dot-circled');
 
+//메인 메뉴
 btn_menu.click(function(e){
   var current_class = $(this).parent().hasClass('menu-act')
   e.preventDefault();
@@ -15,4 +20,10 @@ btn_menu.click(function(e){
   }else{
     $(this).parent().addClass('menu-act');
   }
+});
+// 탭 메뉴
+tab.click(function(e){
+  e.preventDefault();
+  tab_section.removeClass('tab-act');
+  $(this).parent().parent().addClass('tab-act');
 });
